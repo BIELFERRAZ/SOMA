8297c50467ee5b32fdf9b5a907fd3d1da3fc0d2d
Matheus Gabriel <matheusgabrielferraz0802@gmail.com>
2022-04-24 11:07:44 -0300

    Finalizar Tarefa

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int PrimeiroNúmero , SegundoNúmero , Soma ;
+
+Console.WriteLine("Cálcule dois Números:\n");
+
+
+Console.Write("Digite o Primeiro Número:");
+PrimeiroNúmero =Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Digite o Segundo Número:");
+SegundoNúmero = Convert.ToInt32(Console.ReadLine());
+
+
+
+Soma = PrimeiroNúmero + SegundoNúmero;
+
+Console.WriteLine($"{Soma}"!);
+
+
